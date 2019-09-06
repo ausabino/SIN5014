f63b2dcbbd00522d990b02aaab4531899ece0992
Alan Sabino <alan.sabino@usp.br>
2019-09-06 18:58:50 -0300

    Inclusão da interface de usuario
    - Varias modificações
    - Interface

--- codigos/editorimagem.py
+++ codigos/editorimagem.py
@@ -8,7 +8,7 @@
 #         NOTAS: Biblioteca 'openCV' pode apresentar problemas com
 #                distribuições Linux com interface baseada em Qt
 #         AUTOR: Alan U. Sabino <alan.sabino@usp.br>
-#        VERSÃO: 1.0
+#        VERSÃO: 1.1
 #       CRIAÇÃO: 04/09/2019
 #       REVISÃO: 06/09/2019 Alan U. Sabino <alan.sabino@usp.br> (2)
 #                05/09/2019 Alan U. Sabino <alan.sabino@usp.br> (1)
@@ -18,6 +18,17 @@ import cv2 as biblimagem
 import numpy as np
 from matplotlib import pyplot as grafico
 from copy import deepcopy as copiar
+from random import randrange
+
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import messagebox
+
+CANAIS=3
+VALOR_CANAL=256
 
 def histograma(matriz_pixels):
     altura, largura = matriz_pixels.shape[0:2]
@@ -28,7 +39,7 @@ def histograma(matriz_pixels):
                 histograma[cor][matriz_pixels[linha][coluna][cor]] += 1
     return np.matrix(histograma)
 
-def brilho(matriz_pixels, valor):
+def filtro_brilho(matriz_pixels, valor):
     nova_matriz_pixels = np.copy(matriz_pixels)
     altura, largura = nova_matriz_pixels.shape[0:2]
     for linha in range(0, altura):
@@ -72,7 +83,7 @@ def frequencia_acumulada(histograma):
             histograma_acumulado[cor][valor_pixel] =  histograma_acumulado[cor][valor_pixel-1] + np.array(histograma[cor])[0][valor_pixel]
     return np.matrix(histograma_acumulado)
 
-def equalizacao(matriz_pixels):
+def filtro_equalizacao(matriz_pixels):
     nova_matriz_pixels = np.copy(matriz_pixels)
     altura, largura = nova_matriz_pixels.shape[0:2]
     numero_pixels = altura * largura
@@ -86,30 +97,111 @@ def equalizacao(matriz_pixels):
 
 def grafico_histograma(histograma):
     indice = np.arange(histograma[0].shape[1])
-    cores = ['b','g','r', 'c', 'm', 'y', 'k', 'w']
+    cores = ['b','g','r']
     for cor in range(0, CANAIS):
+        grafico.figure(randrange(1000))
         grafico.xlabel('Valor do pixel no canal', fontsize=10)
         grafico.ylabel('Frequência', fontsize=10)
         grafico.title('Histograma da imagem')
         grafico.bar(indice, np.array(histograma[cor])[0], color=cores[cor], alpha=1)
-        grafico.show()
+        grafico.show(block=False)
 
-def exibe_lado(matriz_pixelsA, matriz_pixelsB):
-    figura_dupla = np.hstack((matriz_pixelsA, matriz_pixelsB))
-    biblimagem.imshow('Lado a Lado', figura_dupla)
-    biblimagem.waitKey()
-    grafico.show()
+def selecionar_imagem():
+    global painelA, caminho
+    caminho = filedialog.askopenfilename()
+    if len(caminho) > 0:
+        imagem_original = biblimagem.imread(caminho)
+        imagem = biblimagem.cvtColor(imagem_original, biblimagem.COLOR_BGR2RGB)
+        imagem = Image.fromarray(imagem)
+        imagem = ImageTk.PhotoImage(imagem)
+        ver_histograma = messagebox.askquestion ('Histograma','Deseja ver histograma?')
+        if painelA is None:
+            painelA = Label(image=imagem)
+            painelA.image = imagem
+            painelA.pack(side="left", padx=10, pady=10)
+            if ver_histograma == 'yes':
+                grafico_histograma(histograma(imagem_original))
+        else:
+            painelA.configure(image=imagem)
+            painelA.image = imagem
+            if ver_histograma == 'yes':
+                grafico_histograma(histograma(imagem_original))
+            if painelB is not None:
+                painelB.configure(image='')
 
-imagem = biblimagem.imread("imgs/estacao.jpg", biblimagem.IMREAD_COLOR)
-#imagem = biblimagem.imread("imgs/cidade.jpg", biblimagem.IMREAD_COLOR)
-#imagem = biblimagem.imread("imgs/ruido.jpg", biblimagem.IMREAD_COLOR)
+def mediana():
+    global painelB
+    if len(caminho) > 0:
+        imagem_tratada_original = filtro_mediana(biblimagem.imread(caminho))
+        imagem_tratada = biblimagem.cvtColor(imagem_tratada_original, biblimagem.COLOR_BGR2RGB)
+        imagem_tratada = Image.fromarray(imagem_tratada)
+        imagem_tratada = ImageTk.PhotoImage(imagem_tratada)
+        ver_histograma = messagebox.askquestion ('Histograma','Deseja ver histograma?')
+        if painelB is None:
+            painelB = Label(image=imagem_tratada)
+            painelB.image = imagem_tratada
+            painelB.pack(side="right", padx=10, pady=10)
+            if ver_histograma == 'yes':
+                grafico_histograma(histograma(imagem_tratada_original))
+        else:
+            painelB.configure(image=imagem_tratada)
+            painelB.image = imagem_tratada
+            if ver_histograma == 'yes':
+                grafico_histograma(histograma(imagem_tratada_original))
 
-CANAIS=3
-VALOR_CANAL=256
+def equalizacao():
+    global painelB
+    if len(caminho) > 0:
+        imagem_tratada_original = filtro_equalizacao(biblimagem.imread(caminho))
+        imagem_tratada = biblimagem.cvtColor(imagem_tratada_original, biblimagem.COLOR_BGR2RGB)
+        imagem_tratada = Image.fromarray(imagem_tratada)
+        imagem_tratada = ImageTk.PhotoImage(imagem_tratada)
+        ver_histograma = messagebox.askquestion ('Histograma','Deseja ver histograma?')
+        if painelB is None:
+            painelB = Label(image=imagem_tratada)
+            painelB.image = imagem_tratada
+            painelB.pack(side="right", padx=10, pady=10)
+            if ver_histograma == 'yes':
+                grafico_histograma(histograma(imagem_tratada_original))
+        else:
+            painelB.configure(image=imagem_tratada)
+            painelB.image = imagem_tratada
+            if ver_histograma == 'yes':
+                grafico_histograma(histograma(imagem_tratada_original))
 
-#grafico_histograma(histograma(imagem))
-#exibe_lado(imagem, brilho(imagem, 100))
-#exibe_lado(imagem, filtro_mediana(imagem))
-#grafico_histograma(frequencia_acumulada(histograma(imagem)))
-#exibe_lado(imagem, equalizacao(imagem))
-grafico_histograma(histograma(equalizacao(imagem)))
+def brilho():
+    global painelB
+    if len(caminho) > 0:
+        valor = simpledialog.askstring("Valor de entrada", "Digite valor de alteração do brilho. Valores positivos para clarear e negativos para escurecer.")
+        if valor is not None:
+            imagem_tratada_original = filtro_brilho(biblimagem.imread(caminho), int(valor))
+            imagem_tratada = biblimagem.cvtColor(imagem_tratada_original, biblimagem.COLOR_BGR2RGB)
+            imagem_tratada = Image.fromarray(imagem_tratada)
+            imagem_tratada = ImageTk.PhotoImage(imagem_tratada)
+            ver_histograma = messagebox.askquestion ('Histograma','Deseja ver histograma?')
+            if painelB is None:
+                painelB = Label(image=imagem_tratada)
+                painelB.image = imagem_tratada
+                painelB.pack(side="right", padx=10, pady=10)
+                if ver_histograma == 'yes':
+                    grafico_histograma(histograma(imagem_tratada_original))
+            else:
+                painelB.configure(image=imagem_tratada)
+                painelB.image = imagem_tratada
+                if ver_histograma == 'yes':
+                    grafico_histograma(histograma(imagem_tratada_original))
+
+root = Tk()
+painelA = None
+painelB = None
+
+abrir_imagem = Button(root, text="Selecionar Imagem", command=selecionar_imagem)
+abrir_imagem.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+aplicar_mediana = Button(root, text="Aplicar filtro de mediana", command=mediana)
+aplicar_mediana.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+aplicar_equalizacao = Button(root, text="Aplicar equalização", command=equalizacao)
+aplicar_equalizacao.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+aplicar_brilho = Button(root, text="Aplicar brilho", command=brilho)
+aplicar_brilho.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+root.mainloop()
